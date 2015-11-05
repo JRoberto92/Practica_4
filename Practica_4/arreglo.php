aef613b7e56fdb315160d96332c659791a7bcf0a
Roberto Cabrera <jr_cabrera_f@hotmail.com>
2015-11-05 12:12:23 -0600

    Practica_4

---
+++ Practica_4/arreglo.php
@@ -0,0 +1,45 @@
+<?php
+  $registros=array(
+  0=>array(
+  "id"=>"1",
+  "titulo"=>"cutonala",
+  "Autor"=>"admin"
+  ),
+  1=>array(
+  "id"=>"1",
+  "titulo"=>"c++",
+  "Autor"=>"benito"
+  ),
+  2=>array(
+  "id"=>"1",
+  "titulo"=>"c#",
+  "Autor"=>"chuy"
+  ),
+  3=>array(
+  "id"=>"1",
+  "titulo"=>"java",
+  "Autor"=>"Roberto"
+  ),
+  4=>array(
+  "id"=>"1",
+  "titulo"=>"base de datos",
+  "Autor"=>"oscar"
+  ),
+);
+?>
+<?php foreach ($registros as $registro){ ?>
+<div>
+    <h1>
+	<?php echo $registro["titulo"]; ?>
+<br/>
+	<?php echo $registro["Autor"]; ?>
+    </h1>
+    <p>
+	cutonala: ingenieria en ciencias computacionales(ICCO)
+    </p>
+<div>
+autor
+</div>
+
+</div>
+<?php } ?>
